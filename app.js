0cf77a91e10e4e8592bdb5b1636aafc4344b6b58
Ben Silverman <benjaminsilverman7@gmail.com>
2020-12-14 21:31:33 -0600

    rearranged app to be express app for heroku deployment

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
-const socket = new WebSocket('ws://localhost:8080');
 
-// Listen for messages
-socket.onmessage = ({ data }) => {
-    console.log('Message from server ', data);
-};
+// const socket = io('ws://localhost:8080');
+const socket = io();
+
+socket.on('message', text => {
+    const el = document.createElement('li');
+    el.classList.add('list-group-item');
+    el.innerHTML = text;
+    document.querySelector('ul').appendChild(el);
+});
 
 document.querySelector('button').onclick = () => {
-    socket.send('hello');
+    const text = document.querySelector('input').value;
+    socket.emit('message', text)
 }
--- index.js
+++ index.js
@@ -1,8 +1,34 @@
-const WebSocket = require('ws')
-const server = new WebSocket.Server({port: '8080'})
+const express = require("express");
+const path = require("path");
+// const http = require('http').createServer();
+const port = process.env.PORT || 8080
+const socketIO = require('socket.io');
+// const io = require('socket.io')(http, {
+//     cors: {origin: "*"}
+// });
 
-server.on('connection', socket => {
-    socket.on('message', message => {
-        socket.send(`Got it: ${message}`);
+const server = express()
+    .get('/node_modules*', (req, res) => {
+        console.log(req.originalUrl)
+        res.sendFile(path.join(__dirname + req.originalUrl)) })
+    .get('/app.js', (req, res) => {
+        console.log(req.originalUrl)
+        res.sendFile(path.join(__dirname + req.originalUrl)) })
+    .get('/', (req, res) => {
+        console.log('here' + req.originalUrl)
+        res.sendFile('/index.html', { root: __dirname })
+    })
+    .listen(port, () =>  console.log(`Listening on http://localhost:${port}`))
+
+const io = socketIO(server);
+
+io.on('connection', (socket) => {
+    console.log('a user connected')
+
+    socket.on('message', (message) => {
+        console.log(message);
+        io.emit('message', `${socket.id.substr(0,2)} said ${message}`);
     });
 });
+
+// http.listen(port, () => console.log('listening on http://localhost:' + port));
